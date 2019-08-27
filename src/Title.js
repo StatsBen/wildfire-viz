eed704d2befa56225b75c547978a7234d675d07b
Ben Clark <bclark@avalancheassociation.ca>
2019-08-27 12:52:32 -0700

    Stubbed a component that will contain a feature graphic for the site with title

---
+++ src/Title.js
@@ -0,0 +1,13 @@
+require("react");
+/** @jsx jsx */
+import { css, jsx } from "@emotion/core";
+
+const Title = () => {
+  return (
+    <div css={css``}>
+      <h1 css={css``}>FireViz</h1>
+    </div>
+  );
+};
+
+export default Title;
